da9d32224cc26728491d38cef6e7a4de0ac20adc
Steve Warren <steve@interactivenyc.com>
2018-09-18 13:53:19 -0400

    fixing cat bug

--- bash.js
+++ bash.js
@@ -17,7 +17,13 @@ process.stdin.on('data', (data) => {
     	ls();
     } else if (cmd.substring(0,3) === 'cat') {
         //debugger;
-        cat(cmd.split(' ')[1]);
+        let file = cmd.split(' ')[1];
+        if (file) {
+            cat(file)
+        } else {
+            process.stdout.write('you must supply a FILENAME with the cat command')
+        }
+        
         //console.log('cat: ', cmd);
 
         
